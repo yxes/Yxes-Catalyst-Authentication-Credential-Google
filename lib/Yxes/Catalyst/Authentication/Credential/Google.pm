02ea308494fcd0834e87fe15b1e83d6adc0bb13d
Yxes <yxes@cpan.org>
2013-08-01 16:09:12 -0700

    Updated Changes and Version Information
    - should have been done previously... ooops

--- lib/Yxes/Catalyst/Authentication/Credential/Google.pm
+++ lib/Yxes/Catalyst/Authentication/Credential/Google.pm
@@ -18,11 +18,11 @@ Yxes::Catalyst::Authentication::Credential::Google - OAuth2 credential for Catal
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
